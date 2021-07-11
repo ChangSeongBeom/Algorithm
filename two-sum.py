7ab351b6e993496f058c4f067b9554261b82a77d
changsungbeom <iwsl1234@naver.com>
2021-07-11 23:27:32 +0900

    문제추기

--- two-sum.py
+++ two-sum.py
@@ -9,16 +9,33 @@
 #                     answer.append(j)
 #         return answer
 #
-class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        hash_dict = {}
-        result = []
-        for i, num in enumerate(nums):
-            hash_dict[num] = i
 
-        for i in range(0, len(nums)):
-            if target - nums[i] in hash_dict and i != hash_dict[target - nums[i]]:
-                result.append(i)
-                result.append(hash_dict[target - nums[i]])
-                break
-        return result
+
+# class Solution:
+#     def twoSum(self, nums: List[int], target: int) -> List[int]:
+#         hash_dict = {}
+#         result = []
+#         for i, num in enumerate(nums):
+#             hash_dict[num] = i
+#
+#         for i in range(0, len(nums)):
+#             if target - nums[i] in hash_dict and i != hash_dict[target - nums[i]]:
+#                 result.append(i)
+#                 result.append(hash_dict[target - nums[i]])
+#                 break
+#         return result
+#
+
+#https://www.acmicpc.net/problem/3273
+n=int(input())
+arr=list(map(int,input().split()))
+target=int(input())
+hash_dict={}
+cnt=0
+for i,num in enumerate(arr):
+    hash_dict[num]=i
+
+for i in range(0,len(arr)):
+    if target-arr[i] in hash_dict and  i < hash_dict[target-arr[i]]:
+        cnt+=1
+print(cnt)
